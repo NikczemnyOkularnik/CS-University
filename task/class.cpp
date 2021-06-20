57fae8e1ebeb7aa1eb918f7a88e7a57b871402c5
Katarzyna Strączyńska <katarzyna.a.straczynska@gmail.com>
2021-06-20 21:25:57 +0200

    Adding ideas

--- task/class.cpp
+++ task/class.cpp
@@ -1,1 +1,24 @@
 #include "class.hpp"
+#include <iostream>
+
+
+
+std::ostream &operator<<(std::ostream &out, const Student &feature)
+{
+    out << feature.name_ << " " << feature.surname_ << "," << feature.address_ << "," << feature.index_ << "," << feature.ID_ << "," << feature.gender_ << std::endl;
+    return out;
+}
+
+/*void printDataBase(std::vector(Student)& vec){
+    if () > 0)
+    {
+        for (auto &el : vec)
+        {
+            std::cout << el;
+        }
+    }
+    else
+    {
+        std::cout << "DataBase is empty!";
+    }
+}*/
--- task/class.hpp
+++ task/class.hpp
@@ -3,19 +3,30 @@
 #include <string>
 #include <vector>
 
-enum class Gender
-{
-    Male,
-    Female
-};
+#define GENDER   \
+    etype(Male), \
+        etype(Female)
 
-class Person
+#define etype(x) F_##x
+
+typedef enum class { GENDER } Gender;
+#undef etype
+#define etype(x) #x
+
+static const char *strGender[] = {GENDER};
+const char *enum2str(Gender f)
+{
+    return strGender[static_cast<int>(f)];
+}
+
+class Student
 {
     std::string name_;
     std::string surname_;
     std::string address_;
     int ID_;
     Gender gender_;
+    int index_;
 
 public:
     std::string GetName() { return name_; }
@@ -23,17 +34,23 @@ public:
     std::string GetAddress() { return address_; }
     int GetID() { return ID_; }
     Gender GetGender() { return gender_; }
-};
-
-class Student : public Person
-{
-    int index_;
-
-public:
     int GetIndex() { return index_; }
+
+    void SetName(const std::string& name) { name_ = name; }
+    void SetSurname(const std::string& surname) { surname_ = surname; }
+    void SetAddress(const std::string& address) { address_ = address; }
+    void SetID(const int& ID) { ID_ = ID; }
+    void SetGender(const Gender& gender) { gender_ = gender; }
+    void SetIndex(const int& index) { index_ = index; }
+
+    friend std::ostream &operator<<(std::ostream &out, const Student &person);
 };
 
 class DataBase
 {
-    std::vector<Student> student;
+    std::vector<Student*> students;
+
+public:
+    void printDataBase(std::vector<Student*> &students);
 };
+
