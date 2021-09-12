7f0b189ae4a573c39add911b19da24b213c0ad03
Katarzyna Strączyńska <katarzyna.a.straczynska@gmail.com>
2021-09-12 18:11:41 +0200

    Adding tests for getters & sorting ID

--- task/test/test.cpp
+++ task/test/test.cpp
@@ -51,9 +51,9 @@ SCENARIO("Adding new students to database")
     {
         DataBase *db = new DataBase();
 
-        Student *s = new Student("Stanisław", "Nowak", "Warszawa", 1, 215, Gender::Male);
-        Student *s1 = new Student("Kasia", "Adamiec", "Warszawa", 12, 2145, Gender::Female);
-        Student *s2 = new Student("Zuza", "Kowalska", "Wrocław", 144, 2115, Gender::Female);
+        Student *s = new Student("Stanisław", "Nowak", "Warszawa", 12, 215, Gender::Male);
+        Student *s1 = new Student("Kasia", "Adamiec", "Warszawa", 144, 2145, Gender::Female);
+        Student *s2 = new Student("Zuza", "Kowalska", "Wrocław", 1, 2115, Gender::Female);
         db->add_student(s);
         db->add_student(s1);
         db->add_student(s2);
@@ -79,6 +79,97 @@ SCENARIO("Adding new students to database")
                 REQUIRE(studentsSortedSurnames.size());
                 auto Surname0 = studentsSortedSurnames[0];
                 CHECK(Surname0->GetSurname() == "Adamiec");
+            }
+        }
+
+        WHEN("Sort students by ID number")
+        {
+            std::vector<Student *> studentsSortedID;
+            db->SortStudentsID(studentsSortedID);
+            THEN("ID numbers should be sorted from smallest to the biggest")
+            {
+                REQUIRE(studentsSortedID.size());
+                auto ID0 = studentsSortedID[2];
+                CHECK(ID0->GetID() == 144);
+            }
+        }
+        WHEN("Delete students by ID")
+        {
+            db->DeleteByIndex(215);
+            THEN("Student should be erased from db vector by using ID")
+            {
+                REQUIRE(db->GetStudentViaID(215) == nullptr);
+            }
+        }
+    }
+}
+
+SCENARIO("Checking getters for database")
+{
+    GIVEN("Database and students")
+    {
+        DataBase *db = new DataBase();
+
+        Student *s = new Student("Stanisław", "Nowak", "Poznań", 12, 215, Gender::Male);
+        Student *s1 = new Student("Kasia", "Adamiec", "Warszawa", 144, 2145, Gender::Female);
+        Student *s2 = new Student("Zuza", "Kowalska", "Wrocław", 1, 2115, Gender::Female);
+        Student *s3 = new Student("Karol", "Biskup", "Gdańsk", 37, 2117, Gender::Male);
+        db->add_student(s);
+        db->add_student(s1);
+        db->add_student(s2);
+        db->add_student(s3);
+        REQUIRE(db->GetVectorSize() == 4);
+
+        std::vector<Student *> students;
+        db->SortStudentsSurnames(students);
+        WHEN("GetSurname")
+        {
+            THEN("vector contains Adamiec")
+            {
+                auto Surname0 = students[0];
+                CHECK(Surname0->GetSurname() == "Adamiec");
+            }
+        }
+        WHEN("GetName")
+        {
+            THEN("Vector contains name Kasia")
+            {
+                auto Name0 = students[0];
+                CHECK(Name0->GetName() == "Kasia");
+            }
+        }
+        WHEN("GetAddress")
+        {
+            THEN("Vector contains Gdańsk")
+            {
+                auto Address0 = students[1];
+                CHECK(Address0->GetAddress() == "Gdańsk");
+            }
+        }
+        WHEN("GetID")
+        {
+            THEN("Vector contains 144")
+            {
+                auto ID0 = students[0];
+                CHECK(ID0->GetID() == 144);
+            }
+        }
+        WHEN("GetIndex")
+        {
+            THEN("Vector contains 2145")
+            {
+                auto Index0 = students[0];
+                CHECK(Index0->GetIndex() == 2145);
+            }
+        }
+        WHEN("GetGender")
+        {
+            THEN("Vector contains Gender")
+            {
+                auto Gender0 = students[0];
+                auto Gender1 = students[1];
+                CHECK(Gender0->GetGenderInString() == "Dziewczyna");
+                CHECK(Gender1->GetGenderInString() == "Chłopak");
             }
         }
     }
