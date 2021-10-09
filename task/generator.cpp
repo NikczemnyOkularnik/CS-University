e107f4c44db870e1756fbd918cafd9ca7a337000
Crazyayane07 <crazyayane07@gmail.com>
2021-10-09 19:13:09 +0200

    hot fix

--- task/generator.cpp
+++ task/generator.cpp
@@ -7,15 +7,23 @@
 void Generator::GeneratePersonel(int NumberOfStudentsToGenerate, int NumberOfWorkersToGenerate, std::vector<std::unique_ptr<UniversityPerson>>& person) {
     srand(time(NULL));
 
-    int nameRandId = rand() % NAMES_SIZE;
-    int surnameRandId = rand() % SURNAMES_SIZE;
-    int addressRandId = rand() % ADDRESSES_SIZE;
+    int nameRandId;
+    int surnameRandId;
+    int addressRandId;
 
     for (int i = 0; i < NumberOfStudentsToGenerate; i++) {
+		nameRandId = rand() % NAMES_SIZE;
+    	surnameRandId = rand() % SURNAMES_SIZE;
+    	addressRandId = rand() % ADDRESSES_SIZE;
+
         Student* s = new Student(names_[nameRandId], surnames_[surnameRandId], adresses_[addressRandId], i, i % 2 == 0 ? Gender::Female : Gender::Male, i);
         person.emplace_back(s);
     }
     for (int i = 0; i < NumberOfWorkersToGenerate; i++) {
+		nameRandId = rand() % NAMES_SIZE;
+    	surnameRandId = rand() % SURNAMES_SIZE;
+    	addressRandId = rand() % ADDRESSES_SIZE;
+		
         Worker* w = new Worker(names_[nameRandId], surnames_[surnameRandId], adresses_[addressRandId], i, i % 2 == 0 ? Gender::Female : Gender::Male, i);
         person.emplace_back(w);
     }
