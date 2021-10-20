c1db5f5827e210f85833fd189b6ec31092593047
NikczemnyOkularnik <grzegorzbrodziakgd@gmail.com>
2021-10-20 12:40:29 +0200

    Fixed Generator

--- task/generator.cpp
+++ task/generator.cpp
@@ -17,7 +17,7 @@ void Generator::GeneratePersonel(int NumberOfStudentsToGenerate, int NumberOfWor
     	addressRandId = rand() % ADDRESSES_SIZE;
 
         std::unique_ptr<Student> s = std::make_unique<Student>(names_[nameRandId], surnames_[surnameRandId], adresses_[addressRandId], i, i % 2 == 0 ? Gender::Female : Gender::Male, i);
-        person.emplace_back(s);
+        person.emplace_back(std::move(s));
     }
     for (int i = 0; i < NumberOfWorkersToGenerate; i++) {
 		nameRandId = rand() % NAMES_SIZE;
@@ -25,6 +25,6 @@ void Generator::GeneratePersonel(int NumberOfStudentsToGenerate, int NumberOfWor
     	addressRandId = rand() % ADDRESSES_SIZE;
 		
         std::unique_ptr<Worker> w = std::make_unique<Worker>(names_[nameRandId], surnames_[surnameRandId], adresses_[addressRandId], i, i % 2 == 0 ? Gender::Female : Gender::Male, i);
-        person.emplace_back(w);
+        person.emplace_back(std::move(w));
     }
 }
